eb7fadbf58214b097ba2e1b71479763ce5fcea7d
dusan pejcic <dushan.pejcic@googlemail.com>
2021-07-11 23:58:15 +0200

    smooth scroll to the sections added

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,8 +3,5 @@ import '../sass/styles.scss';
 import { alignNavAndText } from "./hero";
 
 console.log('Webpack Boilerplate');
-/* const width = getNavWidth();
-console.log(width);
-setNavWidth(width); */
 alignNavAndText();
 const fn = () => console.log("Check out bundle.js, this ES6 arrow function was transformed to classic function for older browser support!")
