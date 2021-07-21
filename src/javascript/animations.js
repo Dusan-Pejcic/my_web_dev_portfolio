09fc6109c901ff874095d826af763f0198252e5b
dusan pejcic <dushan.pejcic@googlemail.com>
2021-07-21 13:58:00 +0200

    gsap setup, hero and serveces section animations added

--- src/javascript/animations.js
+++ src/javascript/animations.js
@@ -3,19 +3,70 @@ import { gsap, Power4 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 gsap.registerPlugin(ScrollTrigger);
 
-export const animTest = ()=> {
+export const heroSectionTextAnimation = ()=> {
 
-    const tl = gsap.timeline()
-
-    tl.from('.hero-text-l span', {
-        duration: .4,
+    const heroTextL = gsap.timeline({ defaults: {duration: .4,
         y: -50,
         opacity: 0,
-        stagger: 0.05
-    }).from('.color-field', {
+        stagger: 0.05,
+        ease:Power4.easeOut,} });
+
+    heroTextL.from('.first-row', {
+        
+    }).from('.second-row', {
+        
+    }, '-=.7' ).from('.third-row', {
+        
+    }, '-=.7').from('.fourth-row', {
+        
+    }, '-=.7').from('.color-field', {
         duration: .4,
         y: -200,
-        stagger: .07
+        stagger: .07,
+        ease:Power4.easeOut,
     }, -.1)
 
+}
+
+export const scrollAimations = ()=> {
+/* ____________________ services section fade in */
+    const greyBg = gsap.timeline({defaults: {
+        duration: .4,
+        opacity: 0,
+        ease:Power4.easeOut,
+    }});
+
+    greyBg.from('main', {})
+    .from('.section__header--grey', {}, 0)
+    .from('.first-section__header', {
+        y: 100  
+    }).from('.container--white', {
+        y:100
+    }, '-=.3')
+
+    ScrollTrigger.create({
+        animation: greyBg,
+        trigger: ".first-section__header",
+        start: 'top 80%',
+        end: "+=500"
+    })
+    /* _____________________________ cardServices fade in */
+
+    const cardServices = gsap.timeline({defaults: {
+        duration: .4,
+        opacity: 0,
+        ease:Power4.easeOut,    
+    }})
+
+    cardServices.from('.card__services', {y:100})
+    .from('.services__li-item',{
+        y:20,
+        stagger: .06} )
+    
+    ScrollTrigger.create({
+        animation: cardServices,
+        trigger: ".section__card-container",
+        start: 'top 80%',
+        end: "+=500"
+    })
 }
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,11 +2,14 @@ import '../sass/styles.scss';
 /* import { getNavWidth, setNavWidth } from "./hero"; */
 import { alignNavAndText } from "./hero";
 import { clickMenu, navMenuOnScroll } from "./nav";
-import { animTest } from "./animations";
+import { heroSectionTextAnimation } from "./animations";
+import { scrollAimations } from "./animations";
 
 console.log('Webpack Boilerplate');
 alignNavAndText();
 clickMenu();
 navMenuOnScroll();
-animTest();
+/*  animations  */
+heroSectionTextAnimation();
+scrollAimations();
 const fn = () => console.log("Check out bundle.js, this ES6 arrow function was transformed to classic function for older browser support!")
